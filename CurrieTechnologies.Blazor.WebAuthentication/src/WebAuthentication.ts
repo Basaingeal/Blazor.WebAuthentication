bd645c8d2e6f173e7da552287500a1b43e5048c8
Michael J. Currie <michaeljcurrie136@gmail.com>
2019-06-19 09:09:34 -0500

    initial commit

---
+++ CurrieTechnologies.Blazor.WebAuthentication/src/WebAuthentication.ts
@@ -0,0 +1,11 @@
+﻿declare var DotNet: any;
+
+// tslint:disable-next-line: interface-name
+interface Window {
+  CurrieTechnologies: any;
+}
+
+window.CurrieTechnologies = window.CurrieTechnologies || {};
+window.CurrieTechnologies.Blazor = window.CurrieTechnologies.Blazor || {};
+window.CurrieTechnologies.Blazor.WebAuthentication =
+  window.CurrieTechnologies.Blazor.WebAuthentication || {};
